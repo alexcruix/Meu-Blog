9537a8e0a95b125cbe726e339f1e6ed373aa9b52
Alex de Campos Santos Toledo Camilo <alexcs@outlook.com.br>
2024-01-03 08:15:57 -0300

    Update hobies.php

--- api/hobies.php
+++ api/hobies.php
@@ -15,6 +15,7 @@ if (!isset($_SESSION['usuario_logado'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Alex de Campos Santos Toledo Camilo" />
     <link rel="stylesheet" href="css/style.css">
+    <import { SpeedInsights } from "@vercel/speed-insights/next">
     <title>Meu Blog</title>
 </head>
 <body>

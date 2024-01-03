8b6ce4e00731ec6a26665bb0373ff0cf342ffb88
Alex de Campos Santos Toledo Camilo <alexcs@outlook.com.br>
2024-01-03 15:33:15 -0300

    Update script.js

--- java/script.js
+++ java/script.js
@@ -1,15 +1,15 @@
-function vermais() {
-    var pontos = document.getElementById('pontos');
-    var mais = document.getElementById('mais');
-    var btnVerMais = document.getElementById('btnVerMais');
+function vermais(){
+    let pontos = document.getElementById("pontos");
+    let maisTexto = document.getElementById("mais");
+    let btnVerMais = document.getElementById("btnVerMais");
 
-    if (pontos.style.display === 'none') {
-      pontos.style.display = 'inline';
-      mais.style.display = 'none';
-      btnVerMais.textContent = 'Ver mais';
-    } else {
-      pontos.style.display = 'none';
-      mais.style.display = 'inline';
-      btnVerMais.textContent = 'Ver menos';
+    if (pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maisTexto.style.display="none";
+        btnVerMais.innerHTML="Ver Mais";
+    }else{
+        pontos.style.display="none";
+        maisTexto.style.display="inline";
+        btnVerMais.innerHTML="Ver Menos";
     }
-  }
+}
